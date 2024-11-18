19a00fbe41e08522afd1d914f0f9d588b764c233
Lucas LETT <lucas.lett@gmail.com>
2024-11-18 20:37:38 +0100

    initial commit

---
+++ src/main/java/org/achencraft/achencraftnavigator/Achencraft_Navigator.java
@@ -0,0 +1,17 @@
+package org.achencraft.achencraftnavigator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Achencraft_Navigator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
